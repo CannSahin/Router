d8b2fface32d8380c2eccbaa54e954fc4aaa49cd
CannSahin <cansahin019@gmail.com>
2025-12-15 13:05:49 +0300

    Added useNavigate.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom";
 
 function Home() {
+
+    const navigate=useNavigate();
+
     return (
-        <div>Home</div>
+        <>
+            <div>Home</div>
+            <button onClick={()=>navigate('/mission')}> Go to mission</button>
+        </>
+
     )
 }
 
